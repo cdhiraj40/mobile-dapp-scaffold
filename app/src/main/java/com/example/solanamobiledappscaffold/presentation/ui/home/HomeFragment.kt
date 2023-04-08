849d87a52d940f6d87dfe096772760b45556bc72
Dhiraj Chauhan <Chauhandhiraj40@gmail.com>
2023-04-08 17:08:23 +0530

    refactor: connection status & preferences

--- app/src/main/java/com/example/solanamobiledappscaffold/presentation/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/solanamobiledappscaffold/presentation/ui/home/HomeFragment.kt
@@ -1,6 +1,8 @@
 package com.example.solanamobiledappscaffold.presentation.ui.home
 
 import android.content.Intent
+import android.content.res.ColorStateList
+import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,9 +10,12 @@ import android.view.ViewGroup
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.GuardedBy
+import androidx.core.content.ContextCompat
+import androidx.core.widget.TextViewCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
+import com.example.solanamobiledappscaffold.R
 import com.example.solanamobiledappscaffold.databinding.FragmentHomeBinding
 import com.example.solanamobiledappscaffold.domain.use_case.basic_storage.BasicPublicKeyStorageUseCase
 import dagger.hilt.android.AndroidEntryPoint
@@ -27,6 +32,8 @@ class HomeFragment : Fragment() {
 
     private val viewModel: HomeViewModel by viewModels()
 
+    private lateinit var basicPublicKeyStorageUseCase: BasicPublicKeyStorageUseCase
+
     private val activityResultLauncher: ActivityResultLauncher<Intent> =
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
             intentSender.onActivityComplete()
@@ -38,10 +45,30 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?,
     ): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+
+        basicPublicKeyStorageUseCase = BasicPublicKeyStorageUseCase(requireContext())
+        if (basicPublicKeyStorageUseCase.publicKey != null) {
+            setConnected()
+        } else {
+            setDisconnected()
+        }
+
+        //
+        binding.connectWallet.setOnClickListener {
+            if (binding.connectWallet.text.equals(getString(R.string.connect_wallet))) {
+                viewModel.connectWallet(intentSender)
+            } else {
+//            viewModel.requestAirdrop()
+            }
+        }
+
+        val animDrawable = binding.root.background as AnimationDrawable
+        animDrawable.setEnterFadeDuration(10)
+        animDrawable.setExitFadeDuration(1000)
+        animDrawable.start()
 
         observeViewModel()
-        return root
+        return binding.root
     }
 
     private fun observeViewModel() {
@@ -49,13 +76,36 @@ class HomeFragment : Fragment() {
             with(viewModel) {
                 uiState.collect { uiState ->
                     uiState.wallet?.let {
-                        BasicPublicKeyStorageUseCase(requireContext()).savePublicKey(
-                            it.publicKey,
-                        )
+                        if (basicPublicKeyStorageUseCase.publicKey == null) {
+                            basicPublicKeyStorageUseCase.savePublicKey(
+                                it.publicKey,
+                            )
+                        }
+                        setConnected()
                     }
                 }
             }
         }
+    }
+
+    private fun setConnected() {
+        binding.connectionStatus.text = getString(R.string.connected)
+        TextViewCompat.setCompoundDrawableTintList(
+            binding.connectionStatus,
+            ColorStateList.valueOf(ContextCompat.getColor(requireContext(), R.color.teal))
+        )
+
+        binding.connectWallet.text = getString(R.string.airdrop_sol)
+    }
+
+    private fun setDisconnected() {
+        binding.connectionStatus.text = getString(R.string.not_connected)
+        TextViewCompat.setCompoundDrawableTintList(
+            binding.connectionStatus,
+            ColorStateList.valueOf(ContextCompat.getColor(requireContext(), R.color.red))
+        )
+
+        binding.connectWallet.text = getString(R.string.connect_wallet)
     }
 
     private val intentSender = object : HomeViewModel.StartActivityForResultSender {
