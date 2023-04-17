832bb932f873d7c060c50f21dfb47aec9081af1b
Dhiraj Chauhan <chauhandhiraj40@gmail.com>
2023-04-18 01:16:39 +0530

    refactor: tweet url

--- app/src/main/java/com/example/solanamobiledappscaffold/common/Constants.kt
+++ app/src/main/java/com/example/solanamobiledappscaffold/common/Constants.kt
@@ -9,7 +9,7 @@ object Constants {
     const val dAPP_NAME = "Solana Mobile Dapp Scaffold"
     const val SOLANA_URL = "https://solana.com"
     const val TWITTER_SHARE_URL =
-        "https://twitter.com/intent/tweet?text=There%20is%20no%20better%20time%20to%20build%20consumer%20mobile%20dApps.%0a%0aStart%20building%3A%20&url=https%3A%2F%2Fgithub.com%2Fcdhiraj40%2Fmobile-dapp-scaffold%2F"
+        "https://twitter.com/intent/tweet?text=%22Believe%20me%2C%20there%20is%20no%20better%20time%20to%20build%20consumer%20mobile%20dApps.%22%20-%20%40cdhiraj40%0A%0AStart%20building%20on%20%40solanamobile%20today!&url=https%3A%2F%2Fgithub.com%2Fcdhiraj40%2Fmobile-dapp-scaffold%2F%20"
 
     fun getSolanaExplorerUrl(transactionID: String) =
         "https://explorer.solana.com/tx/$transactionID?cluster=devnet"
